78b00a68c862eca63f12268e67540f216e792fda
nitantjain014 <nitantjain014@gmail.com>
2026-08-25 08:46:23 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,67 @@
+const CACHE_VERSION = 'v5';
+const CACHE_NAME = `schoolsync-${CACHE_VERSION}`;
+
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache).catch(err => {
+                console.log('Cache addAll error:', err);
+            });
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    if (!event.request.url.startsWith(self.location.origin)) {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then((response) => {
+                if (!response || response.status !== 200 || response.type === 'error') {
+                    return response;
+                }
+
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            }).catch(() => {
+                return caches.match('/index.html');
+            });
+        })
+    );
+});
